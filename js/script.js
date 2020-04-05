95acf5abab24cfe5bb190cba5accf50061a8de49
Beastly4 <csgopashabeast@gmail.com>
2020-04-05 16:40:46 +0300

    modified burger, notification form

--- js/script.js
+++ js/script.js
@@ -1,19 +1,40 @@
 window.addEventListener("load", () => {
-    let burgerOff = document.querySelector(".block-header__burger-wrapper__burger");
-    let burgerOffLineTop = document.querySelector(".block-header__burger-wrapper__burger--lt");
-    let burgerOffLineMiddle = document.querySelector(".block-header__burger-wrapper__burger--lm");
-    let burgerOffLineBottom = document.querySelector(".block-header__burger-wrapper__burger--lb");
+    let burgerOff = document.querySelector(".burger-wrapper__burger");
+    let burgerOffLineTop = document.querySelector(".burger-wrapper__burger--lt");
+    let burgerOffLineMiddle = document.querySelector(".burger-wrapper__burger--lm");
+    let burgerOffLineBottom = document.querySelector(".burger-wrapper__burger--lb");
+    const form = document.querySelector(".form");
+    const overlay = document.querySelector(".overlay");
+    const close = document.querySelector(".form__notification__close");
+    const menu = document.querySelector(".block-header__nav");
+    const main = document.querySelector("main");
 
 
 
     burgerOff.onclick = function () {
-        burgerOffLineTop.classList.toggle("block-header__burger-wrapper__burgerOn--ltOn");
-        burgerOffLineMiddle.classList.toggle("block-header__burger-wrapper__burgerOn--lmOn");
-        burgerOffLineBottom.classList.toggle("block-header__burger-wrapper__burgerOn--lbOn");
-        burgerOff.classList.toggle("block-header__burger-wrapper__burgerOn");
+        burgerOffLineTop.classList.toggle("burger-wrapper__burgerOn--ltOn");
+        burgerOffLineMiddle.classList.toggle("burger-wrapper__burgerOn--lmOn");
+        burgerOffLineBottom.classList.toggle("burger-wrapper__burgerOn--lbOn");
+        burgerOff.classList.toggle("burger-wrapper__burgerOn");
+        menu.classList.toggle("menu-active");
+        main.classList.toggle("main-active");
 
 
     }
+
+    setTimeout(() => {
+        form.style.display = "block";
+        overlay.style.display = "block";
+        close.onclick = function () {
+            form.style.display = "";
+            overlay.style.display = "";
+        }
+
+        overlay.onclick = function () {
+            form.style.display = "";
+            overlay.style.display = "";
+        }
+    }, 4000);
 
 
 });
